f94fbe3a4a2115bc1cb207a89743014e40cd6a53
Hoang <hoangtvse62552@fpt.edu.vn>
2019-06-02 15:45:40 +0700

    Save BE

---
+++ InitializrSpringbootProject/src/main/java/com/example/demo/constants/ImageConstant.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.demo.constants;
+
+/**
+ *
+ * @author edu-boot
+ */
+public class ImageConstant {
+    public static final String DEFAULT_IMG = "https://res.cloudinary.com/conchymxinhxinh/image/upload/v1550498243/shopping-web/default-image.png";
+}
---
+++ InitializrSpringbootProject/src/main/java/com/example/demo/security/JWTAuthenticationFilter.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.demo.security;
+
+import com.auth0.jwt.JWT;
+import com.example.demo.entity.User;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+/**
+ *
+ * @author edu-boot
+ */
+import static com.auth0.jwt.algorithms.Algorithm.HMAC512;
+import com.example.demo.constants.SecurityConstant;
+import static com.example.demo.constants.SecurityConstant.EXPIRATION_TIME;
+import static com.example.demo.constants.SecurityConstant.HEADER_STRING;
+import static com.example.demo.constants.SecurityConstant.TOKEN_PREFIX;
+import static com.example.demo.constants.SecurityConstant.SECRET;
+import java.util.List;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private AuthenticationManager authenticationManager;
+
+    public JWTAuthenticationFilter(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest req,
+            HttpServletResponse res) throws AuthenticationException {
+        try {
+            User creds = new ObjectMapper()
+                    .readValue(req.getInputStream(), User.class);
+            List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+            authorities.add(new SimpleGrantedAuthority((""+creds.isAdmin())));
+            
+            return authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            creds.getCustomerID(),
+                            creds.getPassword(),
+                            new ArrayList<>())
+            );
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest req,
+            HttpServletResponse res,
+            FilterChain chain,
+            Authentication auth) throws IOException, ServletException {
+
+        String token = JWT.create()
+                .withSubject(((UserDetails) auth.getPrincipal()).getUsername())
+                .withExpiresAt(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .sign(HMAC512(SECRET.getBytes()));
+        res.addHeader(HEADER_STRING, TOKEN_PREFIX + token);
+        res.addHeader("Access-Control-Expose-Headers", "Access-Token, Uid");
+
+    }
+}
